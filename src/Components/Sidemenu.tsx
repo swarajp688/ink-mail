f3252829742f1382a41295ae7a3c6214273d975a
swarajp688 <swarajp688@gmail.com>
2023-06-15 01:14:10 +0530

    added react-icons and sidemenu Content component

--- src/Components/Sidemenu.tsx
+++ src/Components/Sidemenu.tsx
@@ -1,10 +1,24 @@
 import {PropsWithChildren} from "react";
 import {Sidemenu} from "./sidemenu.types";
-
+import {GiHamburgerMenu} from "react-icons/gi";
 const Sidemenu = ({children}: PropsWithChildren) => {
   return (
     <div className="drawer md:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      {children}
+    </div>
+  );
+};
+
+const SidemenuContent = ({children}: PropsWithChildren) => {
+  return (
+    <div className="drawer-content flex flex-col items-center justify-center">
+      <label
+        htmlFor="my-drawer-2"
+        className="btn btn-primary drawer-button lg:hidden"
+      >
+        <GiHamburgerMenu />
+      </label>
       {children}
     </div>
   );
