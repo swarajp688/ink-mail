e0e4bb147f4adf9a92ab7840c7c65120cf84e911
swarajp688 <swarajp688@gmail.com>
2023-06-15 02:46:13 +0530

    added sidemenu in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import Sidemenu from "./Components/Sidemenu";
+import {SidemenuType} from "./Components/sidemenu.types";
+
+const getMenuItems = (): SidemenuType["menuItems"] => {
+  return [
+    {
+      id: "/",
+      name: "Inbox",
+    },
+    {
+      id: "/primary",
+      name: "Primary",
+    },
+    {
+      id: "/spam",
+      name: "Spam",
+    },
+    {
+      id: "/bin",
+      name: "Bin",
+    },
+  ];
+};
+
 function App() {
-  return <div>app</div>;
+  const menuItems = getMenuItems();
+
+  return (
+    <div>
+      <Sidemenu>
+        <Sidemenu.List menuItems={menuItems}></Sidemenu.List>
+        <Sidemenu.Content></Sidemenu.Content>
+      </Sidemenu>
+    </div>
+  );
 }
 
 export default App;
