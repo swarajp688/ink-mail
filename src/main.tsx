da91df312a48462ee011f831538a7142d8df057c
swarajp688 <swarajp688@gmail.com>
2023-06-15 05:00:03 +0530

    added redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Routes from "./Routes";
+import store from "./Redux/store";
+import {Provider} from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
 );
