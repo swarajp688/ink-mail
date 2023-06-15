f347a67641410e3dc9ebf591a6f0eeb0eff44714
swarajp688 <swarajp688@gmail.com>
2023-06-15 13:24:15 +0530

    added message for mail not found

--- src/Components/MailTable.tsx
+++ src/Components/MailTable.tsx
@@ -5,21 +5,27 @@ const MailTable = ({mails}: {mails: ReduxStateType["visibleEmails"]}) => {
   return (
     <table className="table bg-slate-100">
       <tbody>
-        {mails.map((email) => (
-          <tr
-            key={email.id}
-            className="hover:bg-slate-50 hover:shadow-md hover:rounded-sm cursor-pointer"
-          >
-            <Link to={`mail/${email.id}`}>
-              <td className="max-w-5xl">
-                <p className="truncate mr-10 inline-block w-1/5">
-                  {email.subject}
-                </p>
-                <p className="truncate w-3/4 inline-block">{email.body}</p>
-              </td>
-            </Link>
+        {mails.length ? (
+          mails.map((email) => (
+            <tr
+              key={email.id}
+              className="hover:bg-slate-50 hover:shadow-md hover:rounded-sm cursor-pointer"
+            >
+              <Link to={`mail/${email.id}`}>
+                <td className="max-w-5xl">
+                  <p className="truncate mr-10 inline-block w-1/5">
+                    {email.subject}
+                  </p>
+                  <p className="truncate w-3/4 inline-block">{email.body}</p>
+                </td>
+              </Link>
+            </tr>
+          ))
+        ) : (
+          <tr className="hover:bg-slate-50 hover:shadow-md hover:rounded-sm cursor-pointer">
+            <td>No Mails found</td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
   );
