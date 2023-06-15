59f893059b782eb955cbb39116784bf3fc9c75b0
swarajp688 <swarajp688@gmail.com>
2023-06-15 13:16:47 +0530

    added url query search filter filter

--- src/app/MailListContainer.tsx
+++ src/app/MailListContainer.tsx
@@ -1,20 +1,28 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useParams} from "react-router";
+import {useSearchParams} from "react-router-dom";
 import MailTable from "../Components/MailTable";
 import {setVisibleEmails} from "../Redux/reducer";
 import {RootState} from "../Redux/store";
+import SearchBar from "./SearchBar";
 
 const MailListContainer = () => {
   const {mailBoxId} = useParams();
   const {visibleEmails, emails} = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
+  const [params] = useSearchParams();
+  const [searchText, setSearchText] = useState<string>(
+    params.get("query") || "",
+  );
+
   useEffect(() => {
-    dispatch(setVisibleEmails({tag: mailBoxId}));
-  }, [mailBoxId, emails]);
+    dispatch(setVisibleEmails({tag: mailBoxId, query: searchText}));
+  }, [mailBoxId, emails, searchText]);
 
   return (
     <div className="w-full h-full">
+      <SearchBar searchText={searchText} setSearchText={setSearchText} />
       {mailBoxId && <MailTable mails={visibleEmails} />}
     </div>
   );
