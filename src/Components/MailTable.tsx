a81f883dc29edabb5c1914356f40fb73ac14f725
swarajp688 <swarajp688@gmail.com>
2023-06-15 14:29:44 +0530

    added icon in mail table

--- src/Components/MailTable.tsx
+++ src/Components/MailTable.tsx
@@ -1,24 +1,25 @@
 import {Link} from "react-router-dom";
 import {ReduxStateType} from "../Redux/reduxState.type";
-
+import {AiOutlineSend} from "react-icons/ai";
 const MailTable = ({mails}: {mails: ReduxStateType["visibleEmails"]}) => {
   return (
     <table className="table bg-slate-100">
       <tbody>
         {mails.length ? (
           mails.map((email) => (
-            <tr
-              key={email.id}
-              className="hover:bg-slate-50 hover:shadow-md hover:rounded-sm cursor-pointer"
-            >
-              <Link to={`mail/${email.id}`}>
-                <td className="max-w-5xl">
+            <tr key={email.id} className="">
+              <td className="max-w-5xl p-0 border-b-1">
+                <Link
+                  to={`mail/${email.id}`}
+                  className=" p-4 flex items-center  hover:bg-slate-50 hover:shadow-md hover:rounded-md cursor-pointer"
+                >
+                  <AiOutlineSend className="inline-block mr-4" />
                   <p className="truncate mr-10 inline-block w-1/5">
                     {email.subject}
                   </p>
                   <p className="truncate w-3/4 inline-block">{email.body}</p>
-                </td>
-              </Link>
+                </Link>
+              </td>
             </tr>
           ))
         ) : (
